28d5e6f692cb322e193f783a5726f6d54bdc583e
Jerry Waldorf <jwaldorf@ucsd.edu>
2021-02-13 21:56:28 -0800

    final

--- components/product-item.js
+++ components/product-item.js
@@ -2,8 +2,35 @@
 
 class ProductItem extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<li class="product"> <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200> <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p> <p class="price">$109.95</p> <button onclick="alert('Added to Cart!')">Add to Cart</button> </li>`;
+    this.innerHTML = `<li class="product"> <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200> <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p> <p class="price">$109.95</p> <button id="0" onclick="addCart(this)">Add to Cart</button> </li>`;
   }
 }
 
 customElements.define('product-item', ProductItem);
+
+function addCart(button) {
+
+  let storage = window.localStorage.getItem("cart");
+  if(storage == undefined ) {
+    storage = [];
+  } else {
+    storage = JSON.parse(storage);
+  }
+
+  let text = button.innerHTML;
+  if( text == "Add to Cart" ) {
+    let cartCount = document.getElementById("cart-count").innerHTML;
+    cartCount++;
+    document.getElementById("cart-count").innerHTML = cartCount;
+    button.innerHTML = "Remove from Cart";
+    storage.push(button.id);
+    window.localStorage.setItem("cart", JSON.stringify(storage));
+  } else {
+    let cartCount = document.getElementById("cart-count").innerHTML;
+    cartCount--;
+    document.getElementById("cart-count").innerHTML = cartCount;
+    button.innerHTML = "Add to Cart";
+    storage = storage.filter( item => item !== button.id);
+    window.localStorage.setItem("cart", JSON.stringify(storage));
+  }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,13 @@
 
 window.addEventListener("DOMContentLoaded", () => {
   const storage = window.localStorage.getItem("products");
+  let cart = window.localStorage.getItem("cart");
+  if( cart == undefined ) {
+    cart = [];
+  } else {
+    cart = JSON.parse(cart);
+  }
+  document.getElementById("cart-count").innerHTML = cart.length;
 
   if (storage == undefined) {
     fetch("https://fakestoreapi.com/products")
@@ -25,6 +32,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
           let price = template.children.item(0).children.item(2);
           price.innerHTML = "$" + product.price;
+
+          let button = template.children.item(0).children.item(3);
+          button.setAttribute("id", product.id);
+          if( cart.find(item => item == product.id) ) {
+            button.innerHTML = "Remove from Cart";
+          }
         }
       });
   } else {
@@ -45,10 +58,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
       let price = template.children.item(0).children.item(2);
       price.innerHTML = "$" + product.price;
+
+      let button = template.children.item(0).children.item(3);
+      button.setAttribute("id", product.id);
+      if( cart.find(item => item == product.id) ) {
+        button.innerHTML = "Remove from Cart";
+      }
     }
   }
 });
-
-// window.("", () => {
-
-// });
